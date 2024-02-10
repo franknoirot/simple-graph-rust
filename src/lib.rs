79a63bcfde8f040780c2c9894b9b49e8661e092f
Frank Noirot <frank@kittycad.io>
2024-02-10 16:19:47 -0500

    Start building database.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub mod constants;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn imported_constants() {
-        assert_eq!(
-            constants::DELETE_EDGE,
-            "DELETE FROM edges WHERE source = ? AND target = ?"
-        );
-    }
-}
+mod constants;
+mod database;
+// Nothing here yet, but we'll add some public functions soon!
